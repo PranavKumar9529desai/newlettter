151f4c13bb6065b8758482fd14dc3c41ed0e36ee
PranavKumar9529desai <dpranav7745@gmail.com>
2023-04-29 23:01:00 +0530

    upadted the backend.js

--- backend.js
+++ backend.js
@@ -8,6 +8,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use(express.static("public"));
+// app.use("/public",express.static(__dirname+"/public"));
+
 
 
 
@@ -67,10 +69,10 @@ app.post("/", function(req, res){
   run();
 })
 
+const port = process.env.PORT || 3000;
+app.listen(port,function(){  console.log('starting server at '+port);
+});
 
-app.listen(3001,function(){
-  console.log("server 3000");
-})
 // api keys for mailchimp 28b2058a4a8ac4482f09e5e5fd27f34a-us21
 
 // audience id  61137eb2ab
